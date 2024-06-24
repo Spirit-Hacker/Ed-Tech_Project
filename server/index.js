432580d71e7b06e2cba264dec60d854e29e48e6d
Pranil Dhutraj <pranildhutraj@gmail.com>
2024-06-24 15:20:50 +0530

    cors error

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "https://studynotion-frontend-rosy.vercel.app/",
+        origin: "https://studynotion-frontend-rosy.vercel.app",
         credentials: true,
     })
 );
