a8203b3646b19aa66ffc05e1abf4be5c667d1c6d
Pranil Dhutraj <pranildhutraj@gmail.com>
2024-06-24 15:07:42 +0530

    client cors fix in server

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: "https://studynotion-frontend-rosy.vercel.app/",
         credentials: true,
     })
 );
